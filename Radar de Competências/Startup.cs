261e73f69de5da4c365c3eba6ad1fd5746c1f977
ericksalec <ericksalec@gmail.com>
2019-06-13 17:25:53 -0300

    Limpeza de código.

--- Radar de Competências/Startup.cs
+++ Radar de Competências/Startup.cs
@@ -19,8 +19,6 @@ namespace Radar_de_Competências
             Configuration = configuration;
         }
 
-
-      
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
